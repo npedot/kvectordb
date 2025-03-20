a5e92501aadc5e1610871acec7e8d4002ab44013
npedot <nicola.pedot@google.com>
2025-03-20 11:53:19 +0100

    updates AppName to kvectordb

--- src/main/kotlin/kds/Starter.kt
+++ src/main/kotlin/kds/Starter.kt
@@ -4,40 +4,43 @@ import org.jline.reader.*
 import org.jline.reader.impl.completer.StringsCompleter
 import org.jline.terminal.TerminalBuilder
 import org.jline.reader.EndOfFileException
-import org.jline.reader.UserInterruptException
+
+const val APP_NAME = "kvectordb"
 
 fun main() {
+    val db = VectorDatabase()
+
     val terminal = TerminalBuilder.builder()
-        .name("neuromicrok")
+        .name(APP_NAME)
         .build()
 
     val lineReader = LineReaderBuilder.builder()
-        .completer(StringsCompleter(listOf("/hello", "/exit")))
+        .completer(StringsCompleter(listOf("/hello", "/quit", "/documents")))
         .terminal(terminal)
-
-
-        .appName("neuromicrok")
+        .appName(APP_NAME)
         .build()
 
     while (true) {
         try {
-            val line = lineReader.readLine("neuromicrok> ").trim()
+            val line = lineReader.readLine("$APP_NAME> ").trim()
             if (line.equals("/hello")) {
                 println("Hello, World!")
-            } else if (line.equals("/exit")) {
+            } else if (line.equals("/quit")) {
                 break
-            }
-             else {
+            } else if (line.equals("/documents")) {
+                db.getAllDocuments().forEach {
+                    println("Document ID: ${it.id}, Text: ${it.text}")
+                }
+            } else {
                 println("Unknown command: $line")
-
             }
-        }   catch (e: EndOfFileException) {
-            println("Exiting...")
+        } catch (e: EndOfFileException) {
+            println("Quitting...")
             return
         } catch (e: Exception) {
             println("Error: ${e.message}")
             break
         }
     }
-    println("Exiting...")
+    println("Quitting...")
 }
